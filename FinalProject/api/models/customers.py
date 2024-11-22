993fc2c732a7ed0d40415cfa8815b9d80eace6ef
Abhi3003-18 <abhiramabhi3003@gmail.com>
2024-11-22 15:08:23 -0500

    Add
    pushed

---
+++ FinalProject/api/models/customers.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    customer_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    phone_number = Column(String, nullable=False)
+    address = Column(String)
